b26e93180b259593cc5877ceceb3313f2eab798e
Akhlak Mahmood <amahmoo3@ncsu.edu>
2019-02-03 21:46:59 -0500

    update install.sh to allow spaces in HOME env variable

--- install.sh
+++ install.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
-if [ ! -d "$HOME" ]; then
+if [ ! -d "${HOME}" ]; then
   echo You must have \$HOME set first.
+  exit 1
 fi
 
-mkdir -p $HOME/.wd
-cp -i ./wd/* $HOME/.wd
-cp ./README $HOME/.wd
-if [ -d "$HOME/.wd" ]; then
-  echo Done. Please add the following lines to your .bashrc file:
+mkdir -p "${HOME}/.wd"
+cp -i ./wd/* "${HOME}/.wd"
+cp ./README "${HOME}/.wd"
+if [ -d "${HOME}/.wd" ]; then
+  	echo
+  	echo Done. Please add the following lines to your .bashrc file:
 	echo
-	echo export WDHOME=\$HOME/.wd
+	echo export WDHOME="\${HOME}/.wd"
 	echo source \$WDHOME/wd.sh
 	echo
 	echo Please copy wd.1.gz into your man page directory if you want to use it.
 	echo
 else
-  echo Installation FAILED, could not create $HOME/.wd
+  	echo Installation FAILED, could not create "${HOME}/.wd"
 fi
