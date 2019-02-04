7f1ffe076b8ae506ad08f1a705c8a7417948c513
Akhlak Mahmood <amahmoo3@ncsu.edu>
2019-02-03 21:39:48 -0500

    update to display only upto parent directory in wdl list

--- wd/wd.sh
+++ wd/wd.sh
@@ -198,11 +198,15 @@ wdl()
     (( index = $index + 1 ))
   done < "$(_wd_env_scheme_filename)"
 
+  echo
+
   for j in 0 1 2 3 4 5 6 7 8 9 ; do
     if [[ "${slots[$j]}" != "." ]] ; then
-      echo "${j} ${slots[$j]}"
+      printf "  ${j}  "
+      echo "${slots[$j]}" | awk -F'/' '{print $(NF-1)FS$NF}'
+      # echo "${j} ${slots[$j]}"
     else
-      echo "${j}"
+      echo "  ${j}  "
     fi
     echo
   done
